d817df857ae21c556c8d8a02664bc38761b613ab
Skiedas <Skiedsa@mail.ru>
2020-09-23 18:43:40 +0300

    Add EnemySpawner

---
+++ Assets/Scripts/Enemy/EnemyMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+
+    private int _moveDirection;
+
+    private void Start()
+    {
+        _moveDirection = SetMoveDirection();
+        transform.localScale = new Vector3(-_moveDirection, 1, 1);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(_moveDirection * _moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private int SetMoveDirection()
+    {
+        int moveDirection = Random.Range(0, 2);
+
+        if (moveDirection == 0)
+            moveDirection = -1;
+
+        return moveDirection;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _enemiesTemplate;
+    [SerializeField] private GameObject[] _spawnPoints;
+    [SerializeField] private float _timeBetweenSpawn;
+
+    private float _elapsedTime;
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _timeBetweenSpawn)
+        {
+            _elapsedTime = 0;
+
+            int enemyTemplateNumber = Random.Range(0, _enemiesTemplate.Length);
+
+            Spawn(_enemiesTemplate[enemyTemplateNumber]);
+        }
+    }
+
+    private void Spawn(GameObject enemyTemplate)
+    {
+        int spawnpointNumber = Random.Range(0, _spawnPoints.Length);
+
+        Instantiate(enemyTemplate, _spawnPoints[spawnpointNumber].transform);
+    }
+}
